0be50d49b49f372e8e1fb37b437eef7d61d7959f
gutocbs <gutocbsantos@yahoo.com.br>
2021-07-18 18:07:02 -0300

    Criação de interface e classe de gerenciamento de midias

--- src/utilities/Interfaces/IMediaManager.h
+++ src/utilities/Interfaces/IMediaManager.h
@@ -2,52 +2,21 @@
 #define IMEDIAMANAGER_H
 
 #include <QObject>
-#include <QHash>
-#include <QMetaEnum>
 #include "src/utilities/Enums.h"
 #include "src/base/anime.h"
+#include "src/utilities/singleton.h"
 
 class IMediaManager : QObject{
     Q_OBJECT
-
 public:
+
     virtual QHash<QString, int> getHash(Enums::hashList, int valorInt = 0) = 0;
-    virtual QHash<QString, QString> getHash(Enums::hashList, QString valorQString = "") = 0;
-    virtual QHash<QString, QStringList> getHash(Enums::hashList, QStringList valorQStringList = QStringList()) = 0;
-    //return functions
-    virtual QVector<anime*> getMediaList(Enums::mediaList mediaList, QString searchArgument = "") = 0;
-    virtual QVector<anime*> getSortList(Enums::mediaOrder order, Enums::orderType orderType, Enums::mediaList mediaList) = 0;
+    bool fmudaLista(const QString &rid, const QString &rlista, type rtipo);
+    bool fmudaNota(const QString &rid, const QString &rnota);
+    bool fmudaProgresso(const QString &rid, const QString &rprogresso);
+    bool fdeletedaLista(const QString &rid);
+    bool finsereNomeAlternativo(const QString &rid, const QStringList &rnome);
 
-    virtual QVector<anime*> getAnimeYearlyList(int ano) = 0;
-
-    virtual bool compareMedia(QString oficialTitle, QString englishTitle, QStringList alternativeTitles, QString searchedTitle) = 0;
-    virtual void addToHashList(QString mediaId, Enums::mediaList mediaList, Enums::hashList hashList) = 0;
-
-
-
-protected:
-    Enums::mediaType mediaType;
-    Enums::mediaList mediaList;
-    Enums::hashList hashList;
-
-    QVector<anime*> mediaListCurrent;
-    QVector<anime*> mediaListCompleted;
-    QVector<anime*> mediaListPaused;
-    QVector<anime*> mediaListDropped;
-    QVector<anime*> mediaListPlanning;
-
-    QVector<anime*> mediaListSearch;
-
-    QVector<anime*> mediaListSeasonWinter;
-    QVector<anime*> mediaListSeasonSummer;
-    QVector<anime*> mediaListSeasonFall;
-    QVector<anime*> mediaListSeasonSpring;
-
-    QHash<QString,QString> hashMediaListById;
-    QHash<QString,int> hashMediaIndexById;
-    QHash<QString,QStringList> hashMediaNameById;
-    QHash<QString, QStringList> hashMediaCustomNames;
-    QHash<int,QString> hashMediaListSizeByYear;
+    //TODO - Funções de get da midia - Ex: Get Name
 };
-
 #endif // IMEDIAMANAGER_H
