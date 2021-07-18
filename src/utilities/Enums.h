02451b1a6c76651da4556ead24247524cdc68489
augusto.santos <augusto.santos@celcoin.com.br>
2021-07-17 21:57:52 -0300

    Atualização dos enums e novas funções

--- src/utilities/Enums.h
+++ src/utilities/Enums.h
@@ -2,6 +2,7 @@
 #define ENUMS_H
 
 #include <QObject>
+#include <QMetaEnum>
 
 class Enums : public QObject{
 public:
@@ -9,9 +10,29 @@ public:
     Q_ENUM(hashList)
     enum mediaType{ANIME, MANGA, NOVEL, SEASON};
     Q_ENUM(mediaType)
-    enum mediaList{CURRENT, COMPLETED, PAUSED, DROPPED, PLANNING, SEARCH, WINTER, SUMMER, FALL, SPRING};
+    enum mediaList{CURRENT, COMPLETED, PAUSED, DROPPED, PLANNING, SEARCH, WINTER, SUMMER, FALL, SPRING, YEAR};
     Q_ENUM(mediaList)
+    enum mediaOrder{Title, StartDate, Progress, Score, Type};
+    Q_ENUM(mediaOrder)
+    enum orderType{Ascending, Descending};
+    Q_ENUM(orderType)
     virtual ~Enums() = 0;
+
+    static mediaList QStringToEnum(QString enumName){
+        if(enumName.compare(enumToQString(mediaList::CURRENT))) return mediaList::CURRENT;
+        else if(enumName.compare(enumToQString(mediaList::COMPLETED))) return mediaList::COMPLETED;
+        else if(enumName.compare(enumToQString(mediaList::PAUSED))) return mediaList::PAUSED;
+        else if(enumName.compare(enumToQString(mediaList::DROPPED))) return mediaList::DROPPED;
+        else if(enumName.compare(enumToQString(mediaList::PLANNING))) return mediaList::PLANNING;
+        return mediaList::CURRENT;
+    }
+    static QString enumToQString(hashList enumName){
+        return QMetaEnum::fromType<hashList>().valueToKey(enumName);
+    }
+    static QString enumToQString(mediaList enumName){
+        return QMetaEnum::fromType<mediaList>().valueToKey(enumName);
+    }
+
 };
 
 
