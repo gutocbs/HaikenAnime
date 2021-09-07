a1286f78aa5959889043c74a62f15265b7d462b9
gutocbs <gutocbsantos@yahoo.com.br>
2021-09-07 15:17:44 -0300

    Criação de DTO de midia

---
+++ src/DTO/MediaList.h
@@ -0,0 +1,29 @@
+#ifndef MEDIALIST_H
+#define MEDIALIST_H
+
+#include <QObject>
+#include <QStringList>
+#include <QDate> //Pega data de estreia e season
+#include "src/utilities/Enums.h"
+
+class MediaList : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MediaList(Enums::mediaType type, Enums::mediaList list, int size, QObject *parent = nullptr){
+        Q_UNUSED(parent)
+        this->type = type;
+        this->list = list;
+        this->size = size;
+    }
+    Enums::mediaType type;
+    Enums::mediaList list;
+    int size;
+
+signals:
+
+public slots:
+};
+
+
+#endif // MEDIALIST_H
