985326abae9661be9c78098ed3b4f749d937f4ee
gutocbs <gutocbsantos@yahoo.com.br>
2021-09-11 21:45:41 -0300

    Ajuste de get media

--- src/base/Anime/mediacontroller.cpp
+++ src/base/Anime/mediacontroller.cpp
@@ -12,9 +12,9 @@ MediaController *MediaController::instance()
 }
 
 //Todo - Chamar a função de carregar a lista
+//Sempre pegar as medias daqui, nunca criar novas
 MediaController::MediaController(QObject *parent) : QObject(parent)
 {
-
 }
 
 void MediaController::initializeMedia()
@@ -22,6 +22,12 @@ void MediaController::initializeMedia()
     mediaListManager = new AnimeListManager(this);
     mediaManager = new AnimeManager(this);
     mediaSearchManager = new MediaSearchManager(this);
+    animeListManager = new AnimeListManager(this);
+    mangaListManager = new MangaListManager(this);
+    novelListManager = new NovelListManager(this);
+    animeManager = new AnimeManager(this);
+    mangaManager = new MangaManager(this);
+    novelManager = new NovelManager(this);
 }
 
 //Criar funções de controle de mangas e novels
@@ -29,13 +35,13 @@ void MediaController::setMediaManager(Enums::mediaType mediaType)
 {
     switch (mediaType) {
     case Enums::ANIME:
-        mediaManager = new AnimeManager(this);
+        mediaManager = animeManager->instance();
         break;
     case Enums::MANGA:
-//        mediaManager = new AnimeManager(this);
+        mediaManager = mangaManager->instance();
         break;
     case Enums::NOVEL:
-//        mediaManager = new AnimeManager(this);
+        mediaManager = novelManager->instance();
         break;
     }
 }
@@ -44,19 +50,30 @@ void MediaController::setMediaListManager(Enums::mediaType mediaType)
 {
     switch (mediaType) {
     case Enums::ANIME:
-        mediaListManager = new AnimeListManager(this);
+        mediaListManager = animeListManager->instance();
         break;
     case Enums::MANGA:
-//        mediaListManager = new AnimeListManager(this);
+        mediaListManager = mangaListManager->instance();
         break;
     case Enums::NOVEL:
-//        mediaListManager = new AnimeListManager(this);
+        mediaListManager = novelListManager->instance();
         break;
     }
 }
 
-QPointer<IMediaListManager> MediaController::getMediaListManager()
+QPointer<IMediaListManager> MediaController::getMediaListManager(Enums::mediaType mediaType)
 {
+    switch (mediaType) {
+    case Enums::ANIME:
+        setMediaListManager(Enums::ANIME);
+        break;
+    case Enums::MANGA:
+        setMediaListManager(Enums::MANGA);
+        break;
+    case Enums::NOVEL:
+        setMediaListManager(Enums::NOVEL);
+        break;
+    }
     return mediaListManager;
 }
 
@@ -65,7 +82,18 @@ QPointer<IMediaSearchManager> MediaController::getMediaSearchManager()
     return mediaSearchManager;
 }
 
-QPointer<IMediaManager> MediaController::getMediaManager()
+QPointer<IMediaManager> MediaController::getMediaManager(Enums::mediaType mediaType)
 {
+    switch (mediaType) {
+    case Enums::ANIME:
+        setMediaManager(Enums::ANIME);
+        break;
+    case Enums::MANGA:
+        setMediaManager(Enums::MANGA);
+        break;
+    case Enums::NOVEL:
+        setMediaManager(Enums::NOVEL);
+        break;
+    }
     return mediaManager;
 }
--- src/base/Anime/mediacontroller.h
+++ src/base/Anime/mediacontroller.h
@@ -5,8 +5,12 @@
 #include <QPointer>
 #include "src/utilities/Enums.h"
 #include "src/base/Anime/animelistmanager.h"
+#include "src/base/Manga/mangalistmanager.h"
+#include "src/base/Novel/novellistmanager.h"
 #include "src/base/Anime/mediasearchmanager.h"
 #include "src/base/Anime/animemanager.h"
+#include "src/base/Manga/mangamanager.h"
+#include "src/base/Novel/novelmanager.h"
 #include "src/utilities/singleton.h"
 
 //TODO - Essa classe deverá dar load e save nos animes, além de servir como interface para atualizar os animes no anilist
@@ -20,13 +24,19 @@ public:
     void initializeMedia();
     void setMediaListManager(Enums::mediaType mediaType);
     void setMediaManager(Enums::mediaType mediaType);
-    QPointer<IMediaListManager> getMediaListManager();
+    QPointer<IMediaListManager> getMediaListManager(Enums::mediaType mediaType = Enums::mediaType::NONE);
+    QPointer<IMediaManager> getMediaManager(Enums::mediaType mediaType = Enums::mediaType::NONE);
     QPointer<IMediaSearchManager> getMediaSearchManager();
-    QPointer<IMediaManager> getMediaManager();
 
 signals:
 
 private:
+    QPointer<AnimeListManager> animeListManager;
+    QPointer<MangaListManager> mangaListManager;
+    QPointer<NovelListManager> novelListManager;
+    QPointer<AnimeManager> animeManager;
+    QPointer<MangaManager> mangaManager;
+    QPointer<NovelManager> novelManager;
     QPointer<IMediaListManager> mediaListManager;
     QPointer<IMediaSearchManager> mediaSearchManager;
     QPointer<IMediaManager> mediaManager;
